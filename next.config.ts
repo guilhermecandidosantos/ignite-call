d096cab840e02bc99ee0ec4ad2795b788365916d
Guilherme Santos <guilhermecandido1998@gmail.com>
2025-04-11 18:23:23 -0300

    alter next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  experimental: {
+    optimizePackageImports: ['@phosphor-icons/react'],
+  },
 }
 
 export default nextConfig
